9ccc64743ff0335ac824dc5513ddc66b63144c33
running-machin <60750154+running-machin@users.noreply.github.com>
2020-10-24 21:57:56 +0200

    need to check for bugs

--- BreakthruJava/src/rating.java
+++ BreakthruJava/src/rating.java
@@ -1,5 +1,30 @@
 
 public class rating {
+	static int kingBoard[][]= {
+			{1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000},
+			{1000, 700,700,700,700,700,700,700,700,700,1000},
+			{1000,700,500,500,500,500,500,500,500,700,1000},
+			{1000,700,500,300,300,300,300,300,500,700,1000},
+			{1000,700,500,300,100,100,100,300,500,700,1000},
+			{1000,700,500,300,100,0 ,0 ,100 ,300,500,1000},
+			{1000,700,500,300,100,0 ,0 ,100 ,300,500,1000},
+			{1000,700,500,300,100,100,100,300,500,700,1000},
+			{1000,700,500,500,500,500,500,500,500,700,1000},
+			{1000, 700,700,700,700,700,700,700,700,700,1000},
+			{1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000}};
+	static int kingEndBoard[][]={
+			{0,0,0,0,0,0,0,0,0,0,0},
+	        {0,20,10,10,30,30,30,10,10,20,0},
+	        {0,10,20,30,30,30,30,30,20,10,30},
+	        {0,10,30,30,40,40,40,30,30,10,30},
+	        {0,10,30,30,40,40,40,30,30,10,30},
+	        {0,10,20,30,30,30,30,30,20,10,30},
+	        {0,10,10,0,0,0,0,0,10,10,0},
+	        {0,30,30,0,0,0,0,0,30,30,0},
+	        {0,0,0,0,0,0,0,0,0,0,0},
+	        {0,0,0,0,0,0,0,0,0,0,0},
+	        {0,0,0,0,0,0,0,0,0,0,0},
+	        {0,0,0,0,0,0,0,0,0,0,0}};
 //	static int kingMidBoard[][]={
 //	        {-30,-40,-40,-50,-50,-40,-40,-30},
 //	        {-30,-40,-40,-50,-50,-40,-40,-30},
@@ -10,13 +35,20 @@ public class rating {
 //	        { 20, 20,  0,  0,  0,  0, 20, 20},
 //	        { 20, 30, 10,  0,  0, 10, 30, 20}};
 //}
-	public static int rating() {
-		int counter=0;
+	public static int rating(int list,int depth) {
+		
+		int counter=0, material= rateMaterial();
 		counter+=rateAttack();
 		counter+=rateMaterial();
-		counter+=ratemoveability();
-		counter+=ratepositional();
-		
+		counter+=ratemoveability(list, depth,material);
+		counter+=ratepositional(material);
+		BreakthruEngine.flipBoard();
+		counter-=rateAttack();
+		counter-=rateMaterial();
+		counter-=ratemoveability(list, depth,material);
+		counter-=ratepositional(material);
+		BreakthruEngine.flipBoard();
+		//return -(counter+depth*50);
 		return 0;
 	}
 	public static int rateAttack() {
@@ -24,15 +56,8 @@ public class rating {
 		int tempPostionC=BreakthruEngine.KingPositionC;
 		for(int i=0;i<121;i++) {
 			switch (BreakthruEngine.chessBoard[i/11][i%11]) {
-			case "P":{BreakthruEngine.KingPositionC=i;if (!BreakthruEngine.kingSafe()) {counter-=64;}};	
-			break;
+			
 			case "R":{BreakthruEngine.KingPositionC=i;if (!BreakthruEngine.kingSafe()) {counter-=500;}};
-			break;
-			case "B":{BreakthruEngine.KingPositionC=i;if (!BreakthruEngine.kingSafe()) {counter-=300;}};
-			break;
-			case "K":{BreakthruEngine.KingPositionC=i;if (!BreakthruEngine.kingSafe()) {counter-=300;}};
-			break;
-			case "Q":{BreakthruEngine.KingPositionC=i;if (!BreakthruEngine.kingSafe()) {counter-=900;}};
 			break;
 			}
 		}
@@ -53,11 +78,31 @@ public class rating {
 		return counter;
 	
 	}
-	public static int ratemoveability() {
-		return 0;
+	public static int ratemoveability(int listLength,int depth, int material) {
+		int counter=0;
+		counter+=listLength;//5 point per move
+		if (listLength==0) {//Current side is in checkmate or stalemate
+			if(BreakthruEngine.kingSafe()) {
+				counter+=-200000*depth;
+			} else {
+				counter +=-150000*depth;	
+			}
+		}
+		
+		return counter;
 	}
-	public static int ratepositional() {
-		return 0;
+	public static int ratepositional(int material) {
+		int counter=0;
+        for (int i=0;i<121;i++) {
+            switch (BreakthruEngine.chessBoard[i/11][i%11]) {
+                case "K": counter+=kingBoard[i/11][i%11];
+                    break;
+//                case "R": if (material>=1750) {counter+=kingEndBoard[i/8][i%8]; counter+=BreakthruEngine.possibleK(BreakthruEngine.KingPositionC).length()*10;} else
+//                {/*counter+=kingEndBoard[i/8][i%8];*/ counter+=BreakthruEngine.possibleK(BreakthruEngine.KingPositionC).length()*30;}
+//                break;
+            }
+        }
+        return counter;
 	}
 	
 	
